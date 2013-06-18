8bbb319e27fab6f205107466dbc064413c918f40
Jooho Lee <ljhiyh@gmail.com>
2013-06-18 11:35:01 +0900

    update D_Multiple_Modcluster AppB start script

--- EAP601GA_Multiple_Modcluster/Domain_AppB/start.sh
+++ EAP601GA_Multiple_Modcluster/Domain_AppB/start.sh
@@ -1,11 +1,11 @@
 export EAP_HOME=/home/jooho/Applications/JBoss/EAP/jboss-eap-6.0
 export CONF_HOME=/Jhouse/TestCases/EAP601_Test/D_Multiple_Modcluster/Domain_AppB
 export OFFSET=100
+export DOMAIN_MASTER_ADDR=127.0.0.1
 export MULTICAST_ADDR=230.1.0.2
 export JMS_MULTICAST_ADDR=231.7.0.2
 export MODCLUSTER_MULTICAST_ADDR=224.0.1.105
 export HOST_CONTROLLER_PORT=19999
-
 # JVM Options : Server
 export JAVA_OPTS="-server $JAVA_OPTS"
 
@@ -15,7 +15,7 @@ export JAVA_OPTS=" $JAVA_OPTS -Djboss.default.multicast.address=$MULTICAST_ADDR"
 export JAVA_OPTS=" $JAVA_OPTS -Djboss.messaging.group.address=$JMS_MULTICAST_ADDR"
 export JAVA_OPTS=" $JAVA_OPTS -Djboss.modcluster.multicast.address=$MODCLUSTER_MULTICAST_ADDR"
 export JAVA_OPTS=" $JAVA_OPTS -Djboss.socket.binding.port-offset=$OFFSET"
-
+export JAVA_OPTS=" $JAVA_OPTS -Djboss.domain.master.address=$DOMAIN_MASTER_ADDR"
 
  $EAP_HOME/bin/domain.sh
  
